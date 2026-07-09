14b476dc1d1a9842fb212bf7c1f3271c51db0ca4
choiyui <choiyj220220@gmail.com>
2026-07-09 15:25:41 +0900

    feat: update Home component to use MUI Typography and enhance comment form layout

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,30 @@
+import { Box, Typography, TextField, Button, Divider } from "@mui/material";
+
 function Home() {
   return (
     <>
-      <h2>Home</h2>
+      <Typography variant="h2" component="h2">
+        Home{" "}
+      </Typography>
+
+      <Box component="form" sx={{ mt: 2 }}>
+        <TextField
+          fullWidth
+          label="Comment"
+          placeholder="글을 입력해주세요."
+          type="text"
+          name="comment"
+          variant="outlined"
+          multiline
+          rows={5}
+        />
+        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+          글쓰기
+        </Button>
+      </Box>
+      <Divider sx={{ my: 3 }} />
     </>
   );
 }
+
 export default Home;
