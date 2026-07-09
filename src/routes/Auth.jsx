dc5ab491d54de6e84bdfa309eb2637cb200a3d2c
choiyui <choiyj220220@gmail.com>
2026-07-09 11:30:01 +0900

    feat: implement user registration with Firebase authentication

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
+import { authService } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -7,14 +9,8 @@ function Auth() {
     email: "",
     password: "",
   });
-  console.log(form);
 
-  // const handleChange = e => {
-  //   setForm(prev => ({
-  //     ...prev,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
+  const auth = authService;
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -24,16 +20,35 @@ function Auth() {
     }));
   };
 
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newAccount) {
+      //회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      //로그인
+    }
+  };
   return (
     <>
       <Typography variant="h2" component="h2">
-        Login Form
+        {newAccount ? "회원가입 폼" : "로그인 폼"}
       </Typography>
-      <Box component="form" sx={{ mt: 2 }}>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label="Email address"
-          type="email"
+          type="text"
           name="email"
           variant="outlined"
           onChange={handleChange}
@@ -48,10 +63,11 @@ function Auth() {
           onChange={handleChange}
         />
         <Button sx={{ mt: 2 }} type="submit" variant="contained">
-          Login
+          {newAccount ? "회원가입" : "로그인"}
         </Button>
       </Box>
     </>
   );
 }
+
 export default Auth;
